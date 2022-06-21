d8530d78851f600c2c02511bfd9a326dd05c16e0
gusmalc <gustavo@malcotti.com>
2022-06-21 17:58:45 -0300

    1

---
+++ clase_07/js/scripts.js
@@ -0,0 +1,50 @@
+let btn_1 = document.getElementById('btn_1');
+
+//let btn_1 = document.querySelectorAll('.container');
+
+btn_1.addEventListener('click', function(e){
+
+});
+
+btn_1.addEventListener('click', e=>{
+
+});
+
+// btn_1.onclick = function() {
+//     ///
+// }
+
+btn_1.onclick = e=> {
+    ///
+}
+
+
+
+
+
+function suma(a,b){
+    //alert(a+b);
+    return a+b;
+}
+
+// let resultado = suma(2,3);
+
+// alert(resultado);
+
+let suma = (xx)=>{
+    alert(xx);
+    
+}
+
+let suma2 = xx=>{
+    alert(xx);
+    
+}
+
+let suma3 = (a,b)=>{
+    return a+b;
+    
+}
+
+let suma4 = (a,b)=> a+b;
+    
